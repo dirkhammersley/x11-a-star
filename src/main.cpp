1420511533027f9ec1dba5ca6b19f7bacb5dc5f5
Danaan <danaan.metge@gmail.com>
2022-11-20 22:50:58 -0500

    Add a configurable grid to the window.

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ void getEvents(window::XWindow window){
   
   XNextEvent(window.getDisplay(), &event);
 	
-  if (event.type == Expose && event.xexpose.count == 0) {
+  /*if (event.type == Expose && event.xexpose.count == 0) {
     window.redraw();
-  }
+  }*/
 
   // I guess this is how we get keypresses?
   if (event.type == KeyPress && XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
@@ -37,6 +37,38 @@ void getEvents(window::XWindow window){
   }
 }
 
+StaticGrid::StaticGrid(window::XWindow window) : window_{window}{}
+
+void StaticGrid::drawGrid(int w_cells, int h_cells){
+  int x_start = 50;
+  int y_start = 50;
+  int cell_height = 40;
+  int cell_width = 40;
+
+  window_.redraw();
+  bool should_fill = false;
+  for (int i = 0; i < w_cells; i++){
+    for (int j = 0; j < h_cells; j++){
+      should_fill = !should_fill;
+      drawSquare(x_start + j * cell_height, y_start + i * cell_width, cell_height, cell_width, should_fill);
+    }
+  }
+  XFlush(window_.getDisplay());
+}
+
+void StaticGrid::drawSquare(int x, int y, int w, int h, bool full){
+
+  if (full == true){
+    XFillRectangle(window_.getDisplay(), window_.getWindow(), window_.getGc(), x, y, w, h);
+  }else{
+    XDrawRectangle(window_.getDisplay(), window_.getWindow(), window_.getGc(), x, y, w, h);
+  }
+  /*XDrawLine(window_.getDisplay(), window_.getWindow(), window_.getGc(), x, y, x + w, y);
+  XDrawLine(window_.getDisplay(), window_.getWindow(), window_.getGc(), x, y, x, y - h);
+  XDrawLine(window_.getDisplay(), window_.getWindow(), window_.getGc(), x + w, y, x + w, y - h);
+  XDrawLine(window_.getDisplay(), window_.getWindow(), window_.getGc(), x, y - h, x + w, y - h);*/
+}
+
 template <class T>
 bool isClose(T a, T b, double tol){
   return abs(a - b) < tol;
@@ -51,6 +83,7 @@ int main(int argc, char** argv){
   // Init a window
   window::XWindow t_window;
   t_window.init(window_height_, window_width_);
+  StaticGrid grid(t_window);
 
   // Wait a hot second
   std::this_thread::sleep_for(std::chrono::milliseconds(50));
@@ -61,6 +94,8 @@ int main(int argc, char** argv){
     if (e.type == MapNotify)
       break;
   }
+
+  grid.drawGrid(10, 10);
 
   while(1) {
     //TODO: I think this needs to be in a separate thread.
--- src/main.h
+++ src/main.h
@@ -2,6 +2,18 @@
 
 void getEvents(window::XWindow window);
 
+class StaticGrid{
+  public:
+    StaticGrid(window::XWindow window);
+
+    void drawGrid(int w_cells, int h_cells);
+
+    void drawSquare(int x, int y, int w, int h, bool full);
+
+  private:
+    window::XWindow window_;
+};
+
 // Check equality accounting for floating point error
 template <class T>
 bool isClose(T a, T b, double tol);
